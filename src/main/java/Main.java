372a4159bb38d00ded20c0226945f67d8a4347b0
mopkovka13 <evgeniyneskin13@gmail.com>
2024-11-28 21:35:47 +0300

    Upd: refactor main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,12 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
         ArrayList<PokerHand> hands = new ArrayList<>();
+
         hands.add(new PokerHand("KS 2H 5C JD TD"));
         hands.add(new PokerHand("2C 3C 6C 4C 5C"));
         hands.add(new PokerHand("3C 3C 3C 3C 5D"));
         hands.add(new PokerHand("6C 6C KC KC KC")); // Фулл хаус или флэш? Должен быть фулл хаус, т.к. старше
-        hands.add(new PokerHand("TS JS QD AS ArS"));
+        hands.add(new PokerHand("TS JS QD AS AS"));
         hands.add(new PokerHand("TD JD QS KD AS"));
 
         System.out.println("before ranking");
@@ -16,7 +17,7 @@ public class Main {
             System.out.println(hand);
         }
 
-        hands.sort(Collections.reverseOrder());
+        Collections.sort(hands, Collections.reverseOrder());
 
         System.out.println("\nafter ranking");
         for (PokerHand hand : hands) {
